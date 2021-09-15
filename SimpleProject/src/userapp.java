1ee61f2b84723529bb673676d81a17b3263a6fcb
sreenija <sreenija4363@gmail.com>
2021-09-15 15:25:40 +0530

    3 commiting

--- SimpleProject/src/userapp.java
+++ SimpleProject/src/userapp.java
@@ -2,7 +2,7 @@
 public class userapp {
 public static void main(String args[])
 {
-	Product p=new Product(102,"Mobile",5000);
+	Product p=new Product(103,"Mobile",5000);
 	System.out.print(p);
 }
 }
